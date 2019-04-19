74860718ecde59c953bbcf6e866fc31b28a0821a
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-20 00:53:46 +0200

    bootstrap item cs

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 
 namespace Ixocreate\Cache;
 
+use Ixocreate\Application\Service\ServiceManagerConfigurator;
 use Ixocreate\Cache\Factory\CacheItemPoolSubManagerFactory;
 use Ixocreate\Cache\Factory\CacheManagerFactory;
 use Ixocreate\Cache\Factory\CacheSubManagerFactory;
 
-/** @var \Ixocreate\ServiceManager\ServiceManagerConfigurator $serviceManager */
+/** @var ServiceManagerConfigurator $serviceManager */
+
 $serviceManager->addService(CacheManager::class, CacheManagerFactory::class);
 $serviceManager->addSubManager(CacheItemPoolSubManager::class, CacheItemPoolSubManagerFactory::class);
 $serviceManager->addSubManager(CacheSubManager::class, CacheSubManagerFactory::class);
